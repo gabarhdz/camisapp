d18f64754a72dca85a58357389fa5c6eb183f393
Gabriel Abarca Hernández <gabarhdz@outlook.com>
2025-08-04 13:02:59 -0600

    fix to db connections

--- config/db.js
+++ config/db.js
@@ -1,24 +1,14 @@
-// db.js
-const mongoose = require('mongoose'); // Importamos la librería Mongoose
+const mongoose = require('mongoose');
 
-// URI de conexión a MongoDB (reemplaza los datos según tu configuración)
-const mongoURI = 'mongodb+srv://epoabarca:l9KZMO4bHrL4BES9@abarquiri.llfxqyv.mongodb.net/?retryWrites=true&w=majority&appName=abarquiri';
+const mongoURI = 'mongodb+srv://epoabarca:md0pN2wV7yzzayEp@abarquiri.llfxqyv.mongodb.net/test?retryWrites=true&w=majority&appName=abarquiri&authSource=admin';
 
-// Opciones recomendadas para evitar advertencias
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-};
-
-// Función para conectar a la base de datos
 const connectDB = async () => {
   try {
-    await mongoose.connect(mongoURI, options);
+    await mongoose.connect(mongoURI);
     console.log('✅ Conectado a MongoDB Atlas');
   } catch (err) {
     console.error('❌ Error de conexión:', err);
   }
 };
 
-// Exportamos la función de conexión
 module.exports = connectDB;
